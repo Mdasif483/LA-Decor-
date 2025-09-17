123bc801c29d9f6ec43a9cb91e071267ffea05af
Mohd Asif <hiasif7786@gmail.com>
2025-09-17 21:27:15 +0530

    ablout fix

--- src/components/SubComponent/AboutHero.jsx
+++ src/components/SubComponent/AboutHero.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 function AboutHero() {
   const handleScroll = () => {
-    const section = document.getElementById("contact");
+    const section = document.getElementById("about");
     if (section) {
       section.scrollIntoView({ behavior: "smooth" });
     }
